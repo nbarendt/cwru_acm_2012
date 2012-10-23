6e7aa066450ceaad7d96c0fc9d9a5e03ab327b87
Nick Barendt <nick@barendt.com>
2012-10-23 07:43:12 -0700

    adding simple root view to list courses and enrollment

--- cwru_acm/cwru_acm/urls.py
+++ cwru_acm/cwru_acm/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import ListView
+from roster.models import Course
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -8,6 +10,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'cwru_acm.views.home', name='home'),
     # url(r'^cwru_acm/', include('cwru_acm.foo.urls')),
+
+    url(r'^$', ListView.as_view(model=Course, template_name='course_list.html')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
